bc0c3f99b6c274c6343bfe4d23117d8c4194cde4
Julio Vedovatto <julio@intellstartup.com>
2020-11-10 19:42:31 -0300

    feat(store): auth module added (wip)

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
+import * as modules from '@/store/modules'
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    isLoading: false
+  },
+  mutations: {
+    startLoading(state) {
+      state.isLoading = true
+    },
+    stopLoading(state) {
+      state.isLoading = false
+    }
+  },
   actions: {},
-  modules: {}
+  modules: {
+    ...modules
+  }
 })
