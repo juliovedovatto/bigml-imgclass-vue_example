31f6e39eca11c92f59f9fee6e4e49031c78b3384
Julio Vedovatto <julio@intellstartup.com>
2020-11-10 19:35:45 -0300

    fix(i18n): import path fixed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import App from './App.vue'
 import Vue from 'vue'
 
-import i18n from './i18n'
+import i18n from './plugins/i18n'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
