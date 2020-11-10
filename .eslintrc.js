57d8707c4a4555e9a31739993344317ba965edb2
Julio Vedovatto <julio@intellstartup.com>
2020-11-10 15:09:40 -0300

    ESLint Configuration improved; fixed linter warnings errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,28 +4,37 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'comma-dangle': ['error', 'never'],
+    'sort-imports': ['error'],
+
+    // Vue specific
+    'vue/require-prop-type-constructor': 'error',
+    'vue/require-default-prop': 'error',
+    'vue/prop-name-casing': 'error',
+    'vue/order-in-components': 'error'
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
     }
   ]
-};
+}
