12c3c82f6ea40cffebcec8e15819f965abe64376
Julio Vedovatto <julio@intellstartup.com>
2020-11-10 23:42:35 -0300

    feat(tests): test support added

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
+import TestComponent from '@/components/TestComponent.vue'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
+describe('TestComponent.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(TestComponent, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
