bbaca1396c038fc968c40a0dbcd95e07fafa9713
Julio Vedovatto <julio@intellstartup.com>
2020-11-17 11:21:57 -0300

    fix(test): project initial tests fixed
    - auth layer added to retrieve token

--- src/core/api/__tests__/project.js
+++ src/core/api/__tests__/project.js
@@ -1,27 +1,41 @@
-import Project from '../project'
-
-// TODO: solve circular reference hell (Jest + Node fault)
-// TODO: implemente JWT Auth
+import { Auth, Project } from '../index'
+import { TEST_API_KEY, TEST_API_USER } from '@/core/config'
 
 describe('@/core/api/project', () => {
   let projectId = null
 
+  beforeAll(async () => {
+    const { access } = await Auth.token(TEST_API_USER, TEST_API_KEY)
+
+    Object.defineProperty(window.document, 'cookie', {
+      writable: true,
+      value: `accessToken=${access}`
+    })
+  })
+
   test('Should create a project', async () => {
     const name = 'Test Project'
-    const data = { name }
+    const project = { name }
 
-    const result = await Project.create(data)
+    const { data } = await Project.create(project)
 
-    expect(result).objectContaining({
-      id: expect.any(String),
-      name: expect.stringMatching(name)
-    })
+    expect(data).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: expect.stringMatching(name)
+      })
+    )
 
-    projectId = result?.id || null
+    projectId = data?.id || null
   })
+
   test('Should list projects', async () => {
-    const result = await Project.list()
+    const {
+      data: { results }
+    } = await Project.list()
 
-    expect(result).arrayContaining(expect.objectContaining({ id: projectId }))
+    expect(results).toEqual(expect.arrayContaining([expect.objectContaining({ id: projectId })]))
   })
+
+  // TODO: add more tests
 })
